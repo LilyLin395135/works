d6c8857b0e7f2b9a02f5908a7ffeaf6848d2a1d0
LilyLin395135 <lilylin395135@gmail.com>
2023-02-12 11:51:58 +0800

    memo

--- romanTurnArabicNumber/romanTurnArabicNumber/Program.cs
+++ romanTurnArabicNumber/romanTurnArabicNumber/Program.cs
@@ -10,13 +10,13 @@
 
     };
 
-string romanNumerals = "XCIX";
+string romanNumerals = "V";//這裡放入羅馬數字
 
 char[] romanNoArray = romanNumerals.ToCharArray();
 int result = 0;
 for (int i = 0; i < romanNoArray.Length; i++)
 {
-    int value = dic[romanNoArray[i]]; //我不知道dic裡面放key，他會直接跑value出來
+    int value = dic[romanNoArray[i]]; //學到dic裡面放key，他會直接跑value出來
     if (i < romanNoArray.Length - 1 && value < dic[romanNoArray[i + 1]])//值小於右邊的數字就變負數，最後一個數字沒有其他數字可以比一定是加
     {
         result -= value;
