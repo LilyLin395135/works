c7f679b56d38e03d99c230bcd0b621f19ac6133f
LilyLin395135 <lilylin395135@gmail.com>
2023-02-18 10:09:43 +0800

    寫成方法method

--- romanTurnArabicNumber/romanTurnArabicNumber/Program.cs
+++ romanTurnArabicNumber/romanTurnArabicNumber/Program.cs
@@ -1,4 +1,9 @@
-﻿var dic = new Dictionary<char, int>()
+﻿var result = RomanNumberToArabic("XCIX");
+Console.WriteLine(result);
+//-----------------------------------
+int RomanNumberToArabic(string romanNumber)
+{
+    var dic = new Dictionary<char, int>()
     {
         {'I',1 },
         {'V',5 },
@@ -10,22 +15,29 @@
 
     };
 
-string romanNumerals = "XCIX";//這裡放入羅馬數字
+    char[] romanNoArray = romanNumber.ToCharArray();
+    var result = 0;
+    for (int i = 0; i < romanNoArray.Length; i++)
+    {
+        if (dic.ContainsKey(romanNoArray[i]))
+        {
+            int value = dic[romanNoArray[i]]; //學到dic裡面放key，他會直接跑value出來
+            if (i < romanNoArray.Length - 1 && value < dic[romanNoArray[i + 1]])//值小於右邊的數字就變負數，最後一個數字沒有其他數字可以比一定是加
+            {
+                result -= value;
+            }
+            else
+            {
+                result += value;
+            }
+        }
+        else
+        {
+            throw new KeyNotFoundException($"Invalid Roman numeral character:{romanNoArray[i]}");
+            //Exception例外
+        }
+    }
 
-char[] romanNoArray = romanNumerals.ToCharArray();
-int result = 0;
-for (int i = 0; i < romanNoArray.Length; i++)
-{
-    int value = dic[romanNoArray[i]]; //學到dic裡面放key，他會直接跑value出來
-    if (i < romanNoArray.Length - 1 && value < dic[romanNoArray[i + 1]])//值小於右邊的數字就變負數，最後一個數字沒有其他數字可以比一定是加
-    {
-        result -= value;
-    }
-    else
-    {
-        result += value;
-    }
 }
 
-Console.WriteLine(result);
 
